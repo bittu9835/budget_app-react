f1c5637471af9cf9d1938a0471e37b9f0862050a
bittu9835 <bk2671111@gmail.com>
2023-11-27 18:13:04 +0530

    add

--- src/index.tsx
+++ src/index.tsx
@@ -11,15 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/budget_app-react'>
       <DataProvider>
         <App />
       </DataProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
